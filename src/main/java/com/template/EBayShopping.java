c6780fe6221a94d5d2f00d67c00d213acb546edc
sarangtamhaney <sarangtamhaney@gmail.com>
2022-05-03 12:57:00 +0530

    template pattern

---
+++ src/main/java/com/template/EBayShopping.java
@@ -0,0 +1,58 @@
+package com.template;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class EBayShopping extends ShoppingTemplate{
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private String product;
+
+    @FindBy(id = "gh-ac")
+    private WebElement searchBox;
+
+    @FindBy(id = "gh-btn")
+    private WebElement searchBtn;
+
+    @FindBy(css = "span.s-item__reviews-count")
+    private WebElement item;
+
+    @FindBy(css = "div.display-price")
+    private WebElement price;
+
+    public EBayShopping(WebDriver driver, String product){
+        this.driver = driver;
+        this.product = product;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        PageFactory.initElements(driver, this);
+    }
+
+    @Override
+    public void launchSite() {
+        this.driver.get("https://www.ebay.com/");
+    }
+
+    @Override
+    public void searchForProduct() {
+        this.searchBox.sendKeys(this.product);
+        this.searchBtn.click();
+    }
+
+    @Override
+    public void selectProduct() {
+        this.wait.until((d) -> this.item.isDisplayed());
+        this.item.click();
+    }
+
+    @Override
+    public void buy() {
+        this.wait.until((d) -> this.price.isDisplayed());
+        System.out.println(this.price.getText());
+    }
+}
---
+++ src/main/java/com/template/amazonPages/AmazonProductDescriptionPage.java
@@ -0,0 +1,29 @@
+package com.template.amazonPages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AmazonProductDescriptionPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(css = "div#corePrice_feature_div span:nth-child(2)")
+    private WebElement price;
+
+    public AmazonProductDescriptionPage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        PageFactory.initElements(driver, this);
+    }
+
+    public void buy() {
+        this.wait.until((d) -> this.price.isDisplayed());
+        System.out.println(this.price.getText());
+    }
+
+}
